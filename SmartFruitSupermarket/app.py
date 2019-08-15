fac83a140c45489a2875a4e463f7f28e9366f4b0
苗子 <1353263604@qq.com>
2019-08-15 19:53:34 +0800

    提交忽略的文件

---
+++ SmartFruitSupermarket/app.py
@@ -0,0 +1,18 @@
+from app import create_app, db, cli
+
+from app import create_app, db, cli
+from app.models import User, Commodity, Terminal, Transaction, Order
+
+app = create_app()  # 调用工程函数
+cli.register(app)  # 注册语言翻译的快捷命令
+
+if __name__ == '__main__':
+    # socketio.run(app=app, host='0.0.0.0', debug=True)
+    # 这里不要用 debug 模式这会使启动时 启动两次，这样会使程序保存，因为在TensorFlow-GPU第一次加载了显存，
+    # 第二次就不能分配到显存就会报错
+    # 当调用app.run()的时候，用到了Werkzeug库，它会生成一个子进程，当代码有变动的时候它会自动重启
+    # 如果在run（）里加入参数 use_reloader=False，就会取消这个功能，当然，在以后的代码改动后也不会自动更新了。
+    # 可以查看WERKZEUG_RUN_MAIN环境变量， 默认情况下，调用子程序时，它会被设置为True
+    # WERKZEUG_RUN_MAIN = False
+    # socketio.run(app=app, host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='8888')  # 调试模式
